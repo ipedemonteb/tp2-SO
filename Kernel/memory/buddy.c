35021efadfb74f36b1b51fe1e54a3c02a0ae0e80
jgonzalezcornet <jgonzalezcornet@itba.edu.ar>
2024-11-12 00:06:15 -0300

    fixed buddy mem

--- Kernel/memory/buddy.c
+++ Kernel/memory/buddy.c
@@ -2,10 +2,8 @@
 // PVS-Studio Static Code Analyzer for C, C++ and C#: http://www.viva64.com
 #include "../include/memory_manager.h"
 
-#define MIN_LEVEL 5 // nivel minimo de la memoria (2^5 = 32 bytes)
-#define MAX_ORDER 25 // maximo nivel hardcodeado (por el tamaño de la memoria es el maximo nivel que deberia tener, lo pongo como proteccion x las)
-
-// @ todo: ver si deberia hacer validaciones de los parametros en cada funcion o no
+#define MIN_LEVEL 5
+#define MAX_ORDER 25
 
 typedef struct block_t {
   int8_t order;
@@ -15,7 +13,7 @@ typedef struct block_t {
 
 typedef struct buddy_manager {
   int8_t max_order;
-  block_t * free_blocks[MAX_ORDER];   // arreglo de listas de bloques libres x nivel. en el arreglo hay un solo bloque representativo que arranca la lista
+  block_t * free_blocks[MAX_ORDER];
   uint64_t total_mem;
   uint64_t used_mem;
 } buddy_manager;
@@ -26,8 +24,8 @@ block_t * create_block(void * address, int8_t order) {
   block_t * block = (block_t *) address;
   block->order = order;
   block->status = FREE;
-  block->next = buddy_man.free_blocks[order]; // hago que el next sea el current -> voy agregando "al principio"
-  buddy_man.free_blocks[order] = block;  // me pongo como representante de ese nivel
+  block->next = buddy_man.free_blocks[order];
+  buddy_man.free_blocks[order] = block;
   return block;
 }
 
@@ -63,17 +61,17 @@ void remove_block(block_t * block) {
 
 void split_block(int8_t order) {
   block_t * block = buddy_man.free_blocks[order];
-  remove_block(block); // saco al bloque del nivel en el que estaba (pues ya no existe más ese bloque)
-  block_t * buddy_block = (block_t *) ((void *) block + (1L << (order - 1))); // obtengo el buddy del bloque @check order o order-1
-  create_block((void *) buddy_block, order - 1); // creo el bloque buddy
-  create_block((void *) block, order - 1); // creo el bloque original
+  remove_block(block);
+  block_t * buddy_block = (block_t *) ((void *) block + (1L << (order - 1)));
+  create_block((void *) buddy_block, order - 1);
+  create_block((void *) block, order - 1);
 
   return;
 }
 
-block_t * merge(block_t * block, block_t * buddy_block) { // a esta funcion se la llama ya habiendo chequeado que el buddy este libre y sea del mismo nivel que el block, por lo que tiene que mergear a ciegas
+block_t * merge(block_t * block, block_t * buddy_block) {
   if (block == NULL || buddy_block == NULL) {
-    return NULL; // @todo: handle error
+    return NULL;
   }
 
   block_t * left = block < buddy_block ? block : buddy_block;
@@ -90,7 +88,7 @@ void start_mm() {
       level++;
       current_size *= 2;
   }
-  buddy_man.max_order = level; // me guardo el ultimo nivel real
+  buddy_man.max_order = level;
   buddy_man.total_mem = MEM_SIZE;
   buddy_man.used_mem = 0;
   for (int i=0; i < MAX_ORDER; i++) {
@@ -102,10 +100,9 @@ void start_mm() {
 
 void * my_malloc(uint64_t size) {
   if (size <= 0 || size > MEM_SIZE) {
-    return NULL; // @todo: handle error
+    return NULL;
   }
 
-  // encuentro el orden ideal
   int8_t order = 1;
   int64_t block_size = 2;
   while (block_size < size + sizeof(block_t)) {
@@ -113,11 +110,10 @@ void * my_malloc(uint64_t size) {
     block_size *= 2;
   }
 
-  order = (MIN_LEVEL > order) ? MIN_LEVEL : order; // si el tamaño pedido es menor al tamaño minimo, devuelvo el tamaño minimo
+  order = (MIN_LEVEL > order) ? MIN_LEVEL : order;
 
-  if (buddy_man.free_blocks[order] == NULL) { // no hay definidos bloques libres de ese tamaño (o estan ocupados por ende no estan en la lista o nunca se crearon)
+  if (buddy_man.free_blocks[order] == NULL) {
     uint8_t order_approx = 0;
-    // busco el nivel superior más cercano disponible para luego splittearlo
     for (uint8_t i = order + 1; i <= buddy_man.max_order && order_approx == 0; i++) {
       if (buddy_man.free_blocks[i] != NULL) {
         order_approx = i;
@@ -128,18 +124,16 @@ void * my_malloc(uint64_t size) {
       return NULL;
     }
 
-    // splitteo bloques hasta llegar al orden deseado
     while (order_approx > order) {
       split_block(order_approx);
       order_approx--;
     }
   }
 
-  block_t * block = buddy_man.free_blocks[order]; // el bloque que voy a devolver es el que cree en el level encontrado inicialmente
-  remove_block(block); // saco al bloque del nivel en el que estaba (pues estará ocupado ya que lo devuelvo en el malloc)
-  block->status = OCCUPIED; // todo: ver si esto va o no
+  block_t * block = buddy_man.free_blocks[order]; 
+  remove_block(block);
+  block->status = OCCUPIED; 
 
-  buddy_man.used_mem += (1L << order);
   return (void *)((uint8_t *) block + sizeof(block_t));
 }
 
@@ -170,12 +164,19 @@ void my_free (void * address) {
   }
   create_block((void *) block, block->order);
 
-  buddy_man.used_mem -= (1L << block->order);
   return;
 }
 
 void mem(mem_info * info) {
   info->total = buddy_man.total_mem;
-  info-> free = buddy_man.total_mem - buddy_man.used_mem;
-  info->used = buddy_man.used_mem;
+  info->free = 0;
+  for (uint64_t i = MIN_LEVEL; i < buddy_man.max_order; i++) {
+    block_t * current_block = buddy_man.free_blocks[i];
+    while (current_block != NULL) {
+      info->free += (uint64_t)((uint64_t)1 << i);
+      current_block = current_block->next;
+    }
+  }
+  info->used = info->total - info->free;
+  return;
 }
